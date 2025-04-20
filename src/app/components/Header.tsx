35b1e63c63b0a1230ded45f908880a9936a2f13e
alikhan0616 <ak098boy@gmail.com>
2025-04-20 19:10:03 +0500

    add user authentication using workOS

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,12 +1,29 @@
+import { getSignInUrl, signOut, withAuth } from "@workos-inc/authkit-nextjs";
 import Link from "next/link";
 
-export default function Header() {
+export default async function Header() {
+  const { user } = await withAuth();
+  const signInUrl = await getSignInUrl();
+  const handleSignOut = async () => {
+    'use server';
+    await signOut();
+  }
+  
     return(
         <header className="shadow-lg">
           <div className="container max-w-8xl flex items-center justify-between py-4 px-6 mx-auto">
           <Link href='/' className="font-bold text-md sm:text-xl">HireHub</Link>
         <nav className="flex gap-4 *:py-2 *:px-4 *:rounded-md *:hover:shadow-sm">
-        <Link href={'/login'} className="bg-gray-300">Login</Link>
+          {
+            !user && (
+              <Link href={signInUrl} className="bg-gray-300">Login</Link>
+            )
+          }
+          {
+            user && (  
+                <button onClick={handleSignOut} className="cursor-pointer bg-gray-300">Logout</button>
+            )
+          }
         <Link href={'/new-listing'} className="bg-blue-600 text-white">Post a job</Link>
         </nav>
           </div>
